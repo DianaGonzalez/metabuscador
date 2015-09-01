2af3e8992382762728d7f5df08ec1cbbd15e6f03
DianaGonzalez <dianafolv@hotmail.com>
2015-09-01 11:10:01 -0500

    json

--- metabuscador/spiders/pagina.py
+++ metabuscador/spiders/pagina.py
@@ -23,33 +23,9 @@ class PaginaSpider(CrawlSpider):
         #sel = Selector(response)
         #sites = sel.xpath('//ul[@class="directory-url"]/li')
         #items = []
-         for href in response.css("ul.directory.dir-col > li > a::attr('href')"):
-             url = response.urljoin(href.extract())
-             yield scrapy.Request(url, callback=self.parse_dir_contents)
-
-    def parse_dir_contents(self, response):
-        for sel in response.xpath('//ul/li'):
+       for sel in response.xpath('//ul/li'):
             item = MetabuscadorItem()
             item['title'] = sel.xpath('a/text()').extract()
             item['link'] = sel.xpath('a/@href').extract()
             item['desc'] = sel.xpath('text()').extract()
-                #re('-\s[^\n]*\\r')
-            #print link,title,desc
-            #items.append(item)
             yield item
-        #return items
-            next_page = response.css("ul.navigation > li.next-page > a::attr('href')")
-            if next_page:
-                url = response.urljoin(next_page[0].extract())
-                yield scrapy.Request(url,callback=self.parse_dir_contents)
-
-
-#        for href in response.css("ul.directory.dir-col > li > a::attr('href')"):
-#            url = response.urljoin(href.extract())
-#            yield scrapy.Request(url, callback=self.parse_dir_contents)
-
-#    def parse_dir_contents(self, response):
-#        for sel in response.xpath('//ul/li'):
-#            item = MetabuscadorItem()
-#            item['link'] = sel.xpath('a/@href').extract()
-#            yield item
